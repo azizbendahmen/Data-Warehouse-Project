6cbd6904e9581a329f26d21ee8f42018be65ec0b
BEN DAHMEN Mohamed Aziz <166991772+azizbendahmen@users.noreply.github.com>
2026-07-15 23:46:03 +0100

    Add SQL query for data model connectivity check
    Added a SQL query to check data model connectivity between fact and dimension tables.

--- tests/quality_checks_gold_layer.sql
+++ tests/quality_checks_gold_layer.sql
@@ -1,1 +1,8 @@
-
+-- Check the data model connectivity between fact and dimensions
+select * 
+from Gold.fact_sales f
+left join Gold.dim_customer c
+on c.customer_key = f.customer_key
+left join Gold.dim_product p
+on p.product_key = f.product_key
+where p.product_key is null or c.customer_key is null  
